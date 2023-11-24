82d40df9b62993a68869fca62ff807f2d06f4270
loziamina <lozi.amina.sio@gmail.com>
2023-11-24 15:38:16 +0100

    send it to git

--- sandbox_chess/src/components/Button.jsx
+++ sandbox_chess/src/components/Button.jsx
@@ -1,17 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 
-const Button = ({ isEven, label }) => (
-  <button
-    className={`tile ${
-      isEven ? "bg-blue-200" : "bg-white"
-    } flex items-center justify-center`}
-    style={{ width: "80px", height: "80px" }}>
-    {label && typeof label === "string" ? (
-      <img src={label} alt="piece" />
-    ) : (
-      label
-    )}
-  </button>
-)
+// eslint-disable-next-line complexity
+const Button = ({ isEven, label, rowIndex, colIndex, onMove, selectedPiece }) => {
+  const [isClicked, setIsClicked] = useState(false)
+  const handleClick = () => {
+    setIsClicked(!isClicked)
+
+    if (isClicked) {
+      onMove({ label, rowIndex, colIndex })
+      setIsClicked(false) 
+    }
+  }
+
+  return (
+    <button
+      // eslint-disable-next-line react/no-unknown-property
+      isEven={isEven}
+      className={`tile ${isEven ? "bg-blue-200" : "bg-white"} flex items-center justify-center w-0 h-30 ${
+        isClicked ? "clicked" : ""
+      } ${selectedPiece && selectedPiece.rowIndex === rowIndex && selectedPiece.colIndex === colIndex ? "selected" : ""}`}
+      onClick={handleClick}
+      label={label}
+    ></button>
+  )
+}
+
 
 export default Button
